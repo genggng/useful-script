844faf7b84fcea620df3d0808cb05754744ed3f5
Genggeng <47470904+genggng@users.noreply.github.com>
2022-11-16 22:28:46 +0800

    Update host-proxy.sh

--- host-proxy.sh
+++ host-proxy.sh
@@ -1,9 +1,9 @@
-export windows_host=`./wsg_pc_ip/get_pc_ip.sh 1 | awk '{printf $2}'`
+export windows_host=`cat /etc/resolv.conf|grep nameserver|awk '{print $2}'`
+export HTTP_PROXY=http://$windows_host:7890
 export ALL_PROXY=socks5://$windows_host:7890
-export HTTP_PROXY=$ALL_PROXY
-export http_proxy=$ALL_PROXY
-export HTTPS_PROXY=$ALL_PROXY
-export https_proxy=$ALL_PROXY
+export http_proxy=$HTTP_PROXY
+export HTTPS_PROXY=$HTTP_PROXY
+export https_proxy=$HTTP_PROXY
 
 if [ "`git config --global --get proxy.https`" != "socks5://$windows_host:7890" ]; then
             git config --global proxy.https socks5://$windows_host:7890
